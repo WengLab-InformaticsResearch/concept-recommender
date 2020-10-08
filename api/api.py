8a761d8091cb0d040ab9c99f23b1eae4bc508f08
Junghwan Lee <jl5307@cumc.columbia.edu>
2020-10-08 01:32:28 -0400

    update api.py

--- api/api.py
+++ api/api.py
@@ -1,4 +1,6 @@
 import time
+import pickle
+import numpy as np
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -12,6 +14,22 @@ def get_current_time():
 @app.route('/getRecommend',methods=['POST'])
 def get_recommend_concept():
     arguments = request.json
+    mce_matrix = np.load("./data/mce_matrix_line.npy")
+    concept2id = load_data("./data/concept2id_line.pkl")
+    id2concept = build_id2concept(concept2id)
+
+    summed_vec = np.zeros(mce_matrix.shape[1])
+    for concept in arguments:
+        summed_vec += mce_matrix[concept2id[concept]]
+    summed_vec = normalize_vec(summed_vec)
+
+    index_rank = elemwise_cossim(summed_vec, mce_matrix)
+    nn_index = index_rank.argsort()[-arguments.k:] # retrieve top 20 concepts
+
+    nn_concepts = []
+    for idx, _ in enumerate(nn_index):
+        nn_concepts.append(id2concept[nn_index[-(1+idx)]])
+
     recommend_concept_list = [
         {'conceptId' : 5, 'conceptName' : 'mock concept 5'},
         {'conceptId' : 6, 'conceptName' : 'mock concept 6'},
@@ -28,3 +46,28 @@ def get_search_concept():
         {'conceptId' : 3, 'conceptName' : 'mock concept 3'},
         {'conceptId' : 4, 'conceptName' : 'mock concept 4'}]
     return jsonify(search_concept_list)
+
+def load_data(pklfile):
+    f = open(pklfile, "rb")
+    mydata = pickle.load(f)
+    return mydata
+
+def normalize_vec(vec):
+    return vec / np.linalg.norm(vec, ord=1)
+
+def elemwise_cossim(vec, matrix):
+    noms = np.matmul(vec, matrix.transpose())
+    denoms = np.sqrt(np.sum(np.multiply(matrix, matrix), axis=1)) * np.sqrt(np.sum(np.multiply(vec, vec)))
+    sim_array = noms / denoms
+    
+    #print("nan in cosine similarity array is converted to 0")
+    sim_array = np.nan_to_num(sim_array)
+    return sim_array
+
+def build_id2concept(concept2id):
+    id2concept = dict()
+    unique_concepts = list(concept2id.keys())
+
+    for concept in unique_concepts:
+        id2concept[concept2id[concept]] = concept
+    return id2concept
